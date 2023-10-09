4dbae502913344231cb42cfb1f289ee87058b177
developer2 <developer2@otp.com.co>
2023-10-09 15:02:39 -0400

    Productos

--- modulos/Productos/js/Productos.js
+++ modulos/Productos/js/Productos.js
@@ -1,6 +1,11 @@
 $("#ConsultarItem").on('keyup', function (e) {
 
     let StringText = $("#ConsultarItem").val();
-    rellenarMain('modulos/Productos/controller/Productos_BuscarItemLike.php', StringText, 'modulos/Productos/controller/Productos_BuscarItemLike.php');
+
+    if(StringText.length >= 3){
+
+        rellenarMain('modulos/Productos/controller/Productos_BuscarItemLike.php', StringText, 'modulos/Productos/controller/Productos_BuscarItemLike.php');
+    
+    }
 
 });
